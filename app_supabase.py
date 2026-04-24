f0abc85d460c42c25519a1f6534e1d11f1d85c1f
GoldenMind-2025 <goldenrock.best@gmail.com>
2026-04-24 13:31:49 +0530

    update text

--- app_supabase.py
+++ app_supabase.py
@@ -172,8 +172,8 @@ def main():
                 
                 final_album = ov_album if ov_album else default_album
                 final_date = ov_date if ov_date else date_taken
-                final_album = "Mothiaai Memories"
-                final_date = "26-Apr-26"
+                final_album = "Mothiaai Memories 26 April"
+                final_date = ""
 
                 # 4. Render to Screen
                 html = f"""
